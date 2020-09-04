6f82cb3eafd714c8ff2fb1a282fe3106f50341ef
Simon Bray <32272674+simonbray@users.noreply.github.com>
2020-09-04 11:48:55 +0200

    reactivate link checks

--- .build.sh
+++ .build.sh
@@ -14,6 +14,6 @@ gem install bundler
 bundler install --gemfile _data/Gemfile
 bundle exec --gemfile _data/Gemfile jekyll build
 # Check internal links
-#bundle exec htmlproofer ./_site --check-html --allow-hash-href --assume-extension --disable-external
+bundle exec htmlproofer ./_site --check-html --allow-hash-href --assume-extension --disable-external
 # Check external
-#bundle exec htmlproofer ./_site --check-html --allow-hash-href --assume-extension --external-only
+bundle exec htmlproofer ./_site --check-html --allow-hash-href --assume-extension --external-only
